6caaab06ca34939c6012daaf21fa265dc9fcd359
John Chimaobi <45017704+ijelechimaobi@users.noreply.github.com>
2021-01-06 08:00:15 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const path = require("path");
+const cors = require("cors");
+const io = require("socket.io")(http, {
+  cors: {
+    origin: ["http://localhost:3000", "https://giphy-chat.vercel.app"],
+    credentials: true,
+    methods: ["GET", "POST"],
+  },
+});
+
+const dbConfig = require("./config");
+const port = process.env.PORT || 5000;
+
+const Message = require("./message.model");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(dbConfig.MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Exiting now...", err);
+    process.exit();
+  });
+
+app.use(express.static(path.join(__dirname, "/app.html")));
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
+app.get("/", (req, res) => {
+  res.json({
+    message: "Giphy Chat Server is Running",
+    time: new Date().toString(),
+  });
+});
+
+io.on("connection", (socket) => {
+  const users = [];
+
+  // Get users
+  users.push({ id: socket.id });
+  io.emit("users", { users: users });
+
+  // Get the last 10 messages from the database.
+  Message.find()
+    .sort({ createdAt: -1 })
+    .limit(10)
+    .exec((err, messages) => {
+      if (err) return console.error(err);
+
+      // Send the last messages to the user.
+      socket.emit("init", messages);
+    });
+
+  // Listen to connected users for a new message.
+  socket.on("message", (msg) => {
+    // Create a message with the content and the name of the user.
+    const message = new Message({
+      message: msg.message,
+      gif: msg.gif,
+    });
+
+    // Save the message to the database.
+    message.save((err) => {
+      if (err) return console.error(err);
+    });
+
+    // Notify all other users about a new message.
+    socket.broadcast.emit("push", msg);
+  });
+
+  socket.on("disconnect", (reason) => {
+    // Remove user
+    let index = -1;
+    for (let i = 0; i < users.length; i++) {
+      const user = users[i];
+      if (user.id === socket.id) {
+        index = i;
+      }
+    }
+    // Remove user
+    if (index !== -1) {
+      users.splice(index, 1);
+    }
+    io.emit("users", { users: users });
+  });
+});
+
+http.listen(port, () => {
+  console.log("listening on *:" + port);
+});
